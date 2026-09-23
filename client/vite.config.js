dc8e0624e8475c760250572fcd7334c4d0f84b41
Selvam Thirumal <selvamthirumal22@gmail.com>
2026-09-23 17:20:33 +0530

    Update proxy URL in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'https://avinzbackend-2.onrender.com',
+        target: 'https://avinzbackend-5.onrender.com',
         changeOrigin: true,
         secure: false,
       }
